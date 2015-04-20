beb88c0c38ed5323c6f3c7d5dba726d66f97e43a
sjbgit <sjb_strat@hotmail.com>
2015-04-20 16:33:47 -0500

    gauge and list showing - error on charts page

--- app/scripts/controllers/monitorController.js
+++ app/scripts/controllers/monitorController.js
@@ -6,6 +6,10 @@ angular.module('sbAdminApp')
         $scope.info = 'this is the info';
 
 
+        $scope.machineCounters = [];
+
+
+
         $scope.safeApply = function (fn) {
             var phase = this.$root.$$phase;
             if (phase == '$apply' || phase == '$digest') {
@@ -45,41 +49,116 @@ angular.module('sbAdminApp')
                 $scope.value = counters[0].value;
             });
 
+            var copiedMsg = angular.copy(data);
+            var index = getCounterIndex(data);
+
+            console.log('returned index ' + index);
+
+            if (index === -1) {
+                $scope.machineCounters.push(copiedMsg);
+            }
+            else {
+                $scope.machineCounters[index] = copiedMsg;
+            }
+
+
         });
 
-
-        $scope.value = 1.5;
-        $scope.upperLimit = 6;
-        $scope.lowerLimit = 0;
-        $scope.unit = "kW";
-        $scope.precision = 2;
-        $scope.ranges = [
-            {
-                min: 0,
-                max: 1.5,
-                color: '#DEDEDE'
-            },
-            {
-                min: 1.5,
-                max: 2.5,
-                color: '#8DCA2F'
-            },
-            {
-                min: 2.5,
-                max: 3.5,
-                color: '#FDC702'
-            },
-            {
-                min: 3.5,
-                max: 4.5,
-                color: '#FF7700'
-            },
-            {
-                min: 4.5,
-                max: 6.0,
-                color: '#C50200'
+        function getCounterIndex(counter) {
+            var index = -1;
+            var arrayLength = $scope.machineCounters.length;
+            for (var i = 0; i < arrayLength; i++) {
+                console.log($scope.machineCounters[i].machine);
+                if ($scope.machineCounters[i].machine === counter.machine) {
+                    index = i;
+                    break
+                }
             }
-        ];
+            return index;
+        }
+
+
+        setPerfCounterInfo();
+
+        //setSampleGaugeInfo();
+
+
+        function setPerfCounterInfo() {
+
+            //pass this into directive - with a name and id?
+            $scope.value = 50;
+            $scope.upperLimit = 100;
+            $scope.lowerLimit = 0;
+            $scope.unit = "% CPU";
+            $scope.precision = 0;
+            $scope.ranges = [
+                {
+                    min: 0,
+                    max: 20,
+                    color: '#DEDEDE'
+                },
+                {
+                    min: 20,
+                    max: 40,
+                    color: '#8DCA2F'
+                },
+                {
+                    min: 40,
+                    max: 60,
+                    color: '#FDC702'
+                },
+                {
+                    min: 60,
+                    max: 80,
+                    color: '#FF7700'
+                },
+                {
+                    min: 80,
+                    max: 100,
+                    color: '#C50200'
+                }
+            ];
+
+        }
+
+
+
+        function setSampleGaugeInfo() {
+
+            $scope.value = 1.5;
+            $scope.upperLimit = 6;
+            $scope.lowerLimit = 0;
+            $scope.unit = "kW";
+            $scope.precision = 2;
+            $scope.ranges = [
+                {
+                    min: 0,
+                    max: 1.5,
+                    color: '#DEDEDE'
+                },
+                {
+                    min: 1.5,
+                    max: 2.5,
+                    color: '#8DCA2F'
+                },
+                {
+                    min: 2.5,
+                    max: 3.5,
+                    color: '#FDC702'
+                },
+                {
+                    min: 3.5,
+                    max: 4.5,
+                    color: '#FF7700'
+                },
+                {
+                    min: 4.5,
+                    max: 6.0,
+                    color: '#C50200'
+                }
+            ];
+
+        }
 
 
         /*
