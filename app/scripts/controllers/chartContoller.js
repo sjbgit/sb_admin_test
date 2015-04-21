c076e6904141d0df61b6032dc2ecd976fdebe373
sjbgit <sjb_strat@hotmail.com>
2015-04-21 16:19:38 -0500

    data changes

--- app/scripts/controllers/chartContoller.js
+++ app/scripts/controllers/chartContoller.js
@@ -7,69 +7,96 @@
  * Controller of the sbAdminApp
  */
 angular.module('sbAdminApp')
-  .controller('ChartCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
-		$scope.changeData = function() {
-			var data = [88, 22, 30, 90, 2, 78, 4];
-			$scope.line.data[0] = data;
+    .controller('ChartCtrl', ['$scope', '$timeout', '$interval', function ($scope, $timeout, $interval) {
 
-			$scope.bar.data[0] = [2, 90, 44, 76, 2, 12, 18];
-		}
-    $scope.line = {
-	    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-	    series: ['Series A', 'Series B'],
-	    data: [
-	      [65, 59, 80, 81, 56, 55, 40],
-	      [28, 48, 40, 19, 86, 27, 90]
-	    ],
-	    onClick: function (points, evt) {
-	      console.log(points, evt);
-	    }
-    };
+        //$interval($scope.changeData, 3000, 0, true);
 
-    $scope.bar = {
-	    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-		series: ['Series A', 'Series B'],
+        $scope.changeData = function () {
+            var data = [88, 22, 30, 90, 2, 78, 4];
 
-		data: [
-		   [65, 59, 80, 81, 56, 55, 40],
-		   [28, 48, 40, 19, 86, 27, 90]
-		]
-    	
-    };
+            var value = randomIntFromInterval(0, 100);
 
-    $scope.donut = {
-    	labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
-    	data: [300, 500, 100]
-    };
+            $scope.line.data[0].unshift(value);
+            $scope.line.data[0].pop();
 
-    $scope.radar = {
-    	labels:["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+            var value1 = randomIntFromInterval(0, 100);
 
-    	data:[
-    	    [65, 59, 90, 81, 56, 55, 40],
-    	    [28, 48, 40, 19, 96, 27, 100]
-    	]
-    };
+            $scope.line.data[1].unshift(value1);
+            $scope.line.data[1].pop();
 
-    $scope.pie = {
-    	labels : ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
-    	data : [300, 500, 100]
-    };
+            var value2 = randomIntFromInterval(0, 100);
 
-    $scope.polar = {
-    	labels : ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"],
-    	data : [300, 500, 100, 40, 120]
-    };
+            $scope.line.data[2].unshift(value2);
+            $scope.line.data[2].pop();
 
-    $scope.dynamic = {
-    	labels : ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"],
-    	data : [300, 500, 100, 40, 120],
-    	type : 'PolarArea',
+            //$scope.line.data[0] = data;
 
-    	toggle : function () 
-    	{
-    		this.type = this.type === 'PolarArea' ?
-    	    'Pie' : 'PolarArea';
-		}
-    };
-}]);
+            $scope.bar.data[0] = [2, 90, 44, 76, 2, 12, 18];
+
+            //$scope.$apply();
+        }
+
+        function randomIntFromInterval(min,max)
+        {
+            return Math.floor(Math.random()*(max-min+1)+min);
+        }
+
+        $scope.line = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            series: ['Series A', 'Series B'],
+            data: [
+                [65, 59, 80, 81, 56, 55, 40, 77, 81, 12 ,99, 3],
+                [28, 48, 40, 19, 86, 27, 90, 80, 81, 56, 55, 40],
+                [86, 27, 90, 80, 81, 55, 40, 77, 81, 44, 3, 22]
+            ],
+            onClick: function (points, evt) {
+                console.log(points, evt);
+            }
+        };
+
+        $scope.bar = {
+            labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+            series: ['Series A', 'Series B'],
+
+            data: [
+                [65, 59, 80, 81, 56, 55, 40],
+                [28, 48, 40, 19, 86, 27, 90]
+            ]
+
+        };
+
+        $scope.donut = {
+            labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
+            data: [300, 500, 100]
+        };
+
+        $scope.radar = {
+            labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+
+            data: [
+                [65, 59, 90, 81, 56, 55, 40],
+                [28, 48, 40, 19, 96, 27, 100]
+            ]
+        };
+
+        $scope.pie = {
+            labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
+            data: [300, 500, 100]
+        };
+
+        $scope.polar = {
+            labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"],
+            data: [300, 500, 100, 40, 120]
+        };
+
+        $scope.dynamic = {
+            labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"],
+            data: [300, 500, 100, 40, 120],
+            type: 'PolarArea',
+
+            toggle: function () {
+                this.type = this.type === 'PolarArea' ?
+                    'Pie' : 'PolarArea';
+            }
+        };
+    }]);
